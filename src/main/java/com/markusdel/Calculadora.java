e33e8a15535370195c0943f8d6949268ebf443ad
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-02 09:20:07 -0300

    criando classe de teste ingênuo para calculadora

--- src/main/java/com/markusdel/Calculadora.java
+++ src/main/java/com/markusdel/Calculadora.java
@@ -1,24 +1,19 @@
 package com.markusdel;
 
-public class Calculadora
-{
-    public double soma( double parcela1, double parcela2 )
-    {
+public class Calculadora {
+    public double soma( double parcela1, double parcela2 ) {
         return parcela1 + parcela2;
     }
 
-    public double subtrai( double minuendo, double subtraendo )
-    {
+    public double subtrai( double minuendo, double subtraendo ) {
         return minuendo - subtraendo;
     }
 
-    public double multiplica( double fator1, double fator2 )
-    {
+    public double multiplica( double fator1, double fator2 ) {
         return fator1 * fator2;
     }
 
-    public double divide( double dividendo, double divisor )
-    {
+    public double divide( double dividendo, double divisor ) {
         return dividendo / divisor;
     }
 }
